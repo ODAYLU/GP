2ddf11ead495ba068585cadef6a87cb90bae3905
jenin ahmad <jenin.ahmed.2002@hotmail.com>
2022-06-10 14:25:39 +0300

    asd

--- Migrations/20220608172233_newDBbb.Designer.cs
+++ Migrations/20220608172233_newDBbb.Designer.cs
@@ -10,8 +10,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GP.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20220606095338_f")]
-    partial class f
+    [Migration("20220608172233_newDBbb")]
+    partial class newDBbb
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
