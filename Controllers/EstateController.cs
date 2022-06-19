8f602f10e7167f5daf0cf9c92f5b537c0fe93718
mohammedalsayyed <ma8228354@gamail.com>
2022-06-19 21:56:01 +0300

    Add Opinion

--- Controllers/EstateController.cs
+++ Controllers/EstateController.cs
@@ -615,18 +615,18 @@ namespace GP
                 };
                 await _context.InsertComment(comments);
                 var estate = await services.GetOne(Id);
-                Notification msg = new Notification
-                {
-                    Text = $"{User.Identity.Name}تم التعليق  على عقارك بواسطة ",
-                    Time = DateTime.Now,
-                    ReciverId = estate.UserId,
-                    SenderId = User.FindFirstValue(ClaimTypes.NameIdentifier),
-                    Type = "comment",
-                    IsReaded = (ConnectedUser.IDs.Contains(estate.UserId) ? true : false)
-                };
-                await _notification.InsertNot(msg);
+                //Notification msg = new Notification
+                //{
+                //    Text = $"{User.Identity.Name}تم التعليق  على عقارك بواسطة ",
+                //    Time = DateTime.Now,
+                //    ReciverId = estate.UserId,
+                //    SenderId = User.FindFirstValue(ClaimTypes.NameIdentifier),
+                //    Type = "comment",
+                //    IsReaded = (ConnectedUser.IDs.Contains(estate.UserId) ? true : false)
+                //};
+                //await _notification.InsertNot(msg);
 
-                await _hub.Clients.User(estate.UserId).SendAsync("receiveNotification", msg);
+                //await _hub.Clients.User(estate.UserId).SendAsync("receiveNotification", msg);
                 Id = comments.Id;
 
 
@@ -680,35 +680,35 @@ namespace GP
                     UserId = user.Id,
 
                 };
-                
+
                 await _replaies.InsertReply(replaies);
                 var comment = await _context.GetOne(id);
-                Notification msg = new Notification
-                {
-                    Text = $"{User.Identity.Name}تم  الرد  على تعليقك بواسطة ",
-                    Time = DateTime.Now,
-                    ReciverId = comment.UserId,
-                    SenderId = User.FindFirstValue(ClaimTypes.NameIdentifier),
-                    Type = "Reply",
-                    IdAction = $"{comment.EstateId}",
-                    IsReaded = (ConnectedUser.IDs.Contains(comment.UserId) ? true : false)
-                };
-                await _notification.InsertNot(msg);
+                //Notification msg = new Notification
+                //{
+                //    Text = $"{User.Identity.Name}تم  الرد  على تعليقك بواسطة ",
+                //    Time = DateTime.Now,
+                //    ReciverId = comment.UserId,
+                //    SenderId = User.FindFirstValue(ClaimTypes.NameIdentifier),
+                //    Type = "Reply",
+                //    IdAction = $"{comment.EstateId}",
+                //    IsReaded = (ConnectedUser.IDs.Contains(comment.UserId) ? true : false)
+                //};
+                //await _notification.InsertNot(msg);
 
-                await _hub.Clients.User(comment.UserId).SendAsync("receiveNotification", msg);
+                //await _hub.Clients.User(comment.UserId).SendAsync("receiveNotification", msg);
 
-                Notification msg2 = new Notification
-                {
-                    Text = $"{User.Identity.Name}تم  التعليق  على عقارك بواسطة ",
-                    Time = DateTime.Now,
-                    ReciverId = comment.Estate.UserId,
-                    SenderId = User.FindFirstValue(ClaimTypes.NameIdentifier),
-                    Type = "comment",
-                    IsReaded = (ConnectedUser.IDs.Contains(comment.Estate.UserId) ? true : false)
-                };
-                await _notification.InsertNot(msg);
+                //Notification msg2 = new Notification
+                //{
+                //    Text = $"{User.Identity.Name}تم  التعليق  على عقارك بواسطة ",
+                //    Time = DateTime.Now,
+                //    ReciverId = comment.Estate.UserId,
+                //    SenderId = User.FindFirstValue(ClaimTypes.NameIdentifier),
+                //    Type = "comment",
+                //    IsReaded = (ConnectedUser.IDs.Contains(comment.Estate.UserId) ? true : false)
+                //};
+                //await _notification.InsertNot(msg);
 
-                await _hub.Clients.User(comment.Estate.UserId).SendAsync("receiveNotification", msg2);
+                //await _hub.Clients.User(comment.Estate.UserId).SendAsync("receiveNotification", msg2);
             }
 
             else
