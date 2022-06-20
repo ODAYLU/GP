15b74e912cc4cf529d67058c38b9df051b3929db
jenin ahmad <jenin.ahmed.2002@hotmail.com>
2022-06-20 18:30:00 +0300

    o

--- Migrations/20220620151536_ConDB.cs
+++ Migrations/20220620151536_ConDB.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace GP.Migrations
 {
-    public partial class DBContext : Migration
+    public partial class ConDB : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
