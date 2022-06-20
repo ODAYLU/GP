d2b5f39ec25398614515256989dff4fd353c44b8
mohammedalsayyed <ma8228354@gamail.com>
2022-06-20 12:40:16 +0300

    لبلب

--- Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using GP.Models;
+﻿using GP.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 namespace GP.Areas.Identity.Pages.Account.Manage
 {
     public class ChangePasswordModel : PageModel
@@ -16,7 +13,7 @@ namespace GP.Areas.Identity.Pages.Account.Manage
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ILogger<ChangePasswordModel> _logger;
 
-    
+
         public ChangePasswordModel(
             UserManager<AppUser> userManager,
             SignInManager<AppUser> signInManager,
@@ -35,20 +32,21 @@ namespace GP.Areas.Identity.Pages.Account.Manage
 
         public class InputModel
         {
-            [Required]
+            [Required(ErrorMessage = "الحقل مطلوب")]
             [DataType(DataType.Password)]
-            [Display(Name = "Current password")]
+            [Display(Name = "كلمة المرور الحالية")]
             public string OldPassword { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [Required(ErrorMessage = "الحقل مطلوب")]
+            [StringLength(8, ErrorMessage = "يجب أن تحتوي على 8 أحرف", MinimumLength = 8)]
             [DataType(DataType.Password)]
-            [Display(Name = "New password")]
+            [Display(Name = "كلمة المرور الجديدة")]
             public string NewPassword { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm new password")]
-            [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+            [Display(Name = "تأكيد كلمة المرور")]
+            [StringLength(8)]
+            [Compare("NewPassword", ErrorMessage = "كلمتان المرور غير متطابقتان ")]
             public string ConfirmPassword { get; set; }
         }
 
@@ -85,16 +83,18 @@ namespace GP.Areas.Identity.Pages.Account.Manage
             var changePasswordResult = await _userManager.ChangePasswordAsync(user, Input.OldPassword, Input.NewPassword);
             if (!changePasswordResult.Succeeded)
             {
-                foreach (var error in changePasswordResult.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
+                ModelState.AddModelError(string.Empty, "كلمة المرور الحالية غير صحيحة");
+
+                //foreach (var error in changePasswordResult.Errors)
+                //{
+                //    ModelState.AddModelError(string.Empty, error.Description);
+                //}
                 return Page();
             }
 
             await _signInManager.RefreshSignInAsync(user);
             _logger.LogInformation("User changed their password successfully.");
-            StatusMessage = "Your password has been changed.";
+            StatusMessage = "تم تحديث كلمة المرور";
 
             return RedirectToPage();
         }
