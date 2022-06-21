c403b9bd6c43cfa58989fd804e010c43a8a3f1b2
ODAYLU <odaytareqlobad@gmail.com>
2022-06-22 01:08:37 +0300

    ads

--- Controllers/ApiEstateController.cs
+++ Controllers/ApiEstateController.cs
@@ -51,6 +51,10 @@ namespace GP.Controllers
             x.publish &&
             !x.IsBlock);
             var data = await estates.Skip(pageNumber * 20).Take(20).ToListAsync();
+            if(data.Count() == 0)
+            {
+                data = await estates.Skip((pageNumber-1) * 20).Take(20).ToListAsync();
+            }
             decimal total = estates.Count();
             decimal totalDecimal = total / 20;
             int pages = (int)Math.Ceiling(totalDecimal);
@@ -67,6 +71,10 @@ namespace GP.Controllers
             x.publish &&
             !x.IsBlock);
             var data = await estates.Skip(pageNumber * 20).Take(20).ToListAsync();
+            if (data.Count() == 0)
+            {
+                data = await estates.Skip((pageNumber - 1) * 20).Take(20).ToListAsync();
+            }
             decimal total = estates.Count();
             decimal totalDecimal = total / 20;
             int pages = (int)Math.Ceiling(totalDecimal);
@@ -83,6 +91,10 @@ namespace GP.Controllers
             x.publish &&
             !x.IsBlock);
             var data = await estates.Skip(pageNumber * 20).Take(20).ToListAsync();
+            if (data.Count() == 0)
+            {
+                data = await estates.Skip((pageNumber - 1) * 20).Take(20).ToListAsync();
+            }
             decimal total = estates.Count();
             decimal totalDecimal = total / 20;
             int pages = (int)Math.Ceiling(totalDecimal);
