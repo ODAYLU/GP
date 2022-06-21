dc01e71aed3aa2e8c4c2a09a0ec89daedb37ca96
ODAYLU <odaytareqlobad@gmail.com>
2022-06-21 23:36:14 +0300

    asd

--- Controllers/ApiEstateController.cs
+++ Controllers/ApiEstateController.cs
@@ -29,7 +29,12 @@ namespace GP.Controllers
             x.is_active &&
             x.publish &&
             !x.IsBlock);
+            
             var data = await estates.Skip(pageNumber * 20).Take(20).ToListAsync();
+            if(data.Count() == 0)
+            {
+                data = await estates.Skip((pageNumber-1) * 20).Take(20).ToListAsync();
+            }
             decimal total = estates.Count();
             decimal totalDecimal = total / 20;
             int pages = (int)Math.Ceiling(totalDecimal);
