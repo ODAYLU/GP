7a43d19d31f2dadf4b6fe6d0e087c2ee53d44efc
ODAYLU <odaytareqlobad@gmail.com>
2022-06-22 08:32:34 +0300

    asd

---
+++ Migrations/20220622053158_asd.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GP.Migrations
+{
+    public partial class asd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
